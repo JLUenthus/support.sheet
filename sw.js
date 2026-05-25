10d13bd06017a075d733145d044604af81ffecd1
JLUenthus <jan-lukas.lueck@enthus.de>
2026-05-25 12:07:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // AdminSheet – Service Worker v2
 // ============================================================
-const CACHE_VERSION = '20260525-0942';
+const CACHE_VERSION = '20260525-1002';
 const CACHE_NAME = `adminsheet-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -13,6 +13,8 @@ const ASSETS = [
   './eventlog.html',
   './data/eventlog-rules.json',
   './data/commands.json',
+  './data/forti-commands.json',
+  './data/exchange-commands.json',
   './nav.js',
   './sw.js',
   './manifest.json',
