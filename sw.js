8dd8b207cc70178e512059b6e7e6d8c3c5ae9cdc
JLUenthus <jan-lukas.lueck@enthus.de>
2026-07-16 07:55:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // support.sheet – Service Worker v2
 // ============================================================
-const CACHE_VERSION = '20260715-1153';
+const CACHE_VERSION = '20260716-0553';
 const CACHE_NAME = `support.sheet-${CACHE_VERSION}`;
 
 const ASSETS = [
