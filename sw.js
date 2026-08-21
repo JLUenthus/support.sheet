b5947f39d34b4620bb1183e0192af7d79a696cda
JLUenthus <jan-lukas.lueck@enthus.de>
2026-08-21 14:18:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ===========================================================
 // support.sheet – Service Worker v2
 // ===========================================================
-const CACHE_VERSION = '20260821-cis-v52c5-win11cis';
+const CACHE_VERSION = '20260821-cis-v52c6-win11cis';
 const CACHE_NAME = `support.sheet-${CACHE_VERSION}`;
 
 const ASSETS = [
